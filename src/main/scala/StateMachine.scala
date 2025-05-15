8623f84d34c7003bdedcc96ec3cae2d14c456635
Hannes <hannes10042004@icloud.com>
2025-05-15 15:46:21 +0200

    Add proper initial state parsing

--- src/main/scala/StateMachine.scala
+++ src/main/scala/StateMachine.scala
@@ -11,18 +11,13 @@ object StateMachine:
 
       val tree = bodyExprs.asTerm
 
-      println(tree.show(using Printer.TreeStructure))
-    
       val statements = tree match
-        case Inlined(_, _, Block(stats, _)) => stats
+        case Inlined(_, _, Block(stats, e)) =>  {
+          stats :+ e
+        }
         case Inlined(_, _, stat)            => List(stat)
         case Block(stats, _)                => stats
         case _                              => List(tree)
-
-      statements.foreach { stmt =>
-        println(stmt.show(using Printer.TreeStructure))  // For structural tree
-
-}
     
       val states = scala.collection.mutable.ListBuffer[String]()
       var init_state: Option[String] = None
@@ -32,20 +27,22 @@ object StateMachine:
           states += name
     
         case Inlined(Some(Apply(Ident("initialState"), List(Literal(StringConstant(name))))), _, _) =>
+          if init_state.isDefined then
+            report.error(s"Duplicate initial state: $name")
           init_state = Some(name)
-          println("hello")
     
         case Apply(Ident("state"), List(Literal(StringConstant(name)))) =>
           states += name
     
         case Apply(Ident("initialState"), List(Literal(StringConstant(name)))) =>
           init_state = Some(name)
-          println("hello")    
+          if init_state.isDefined then
+            report.error(s"Duplicate initial state: $name")
+
         case stmt =>
           report.warning(s"[macro] Unknown statement: ${stmt.show(using Printer.TreeStructure)}")
       }
     
-      // Convert results into Exprs
       val stateExprs = states.map(Expr(_)).toList
       val allStatesList = Expr.ofList(stateExprs)
       val initialExpr = Expr(init_state.getOrElse(""))
@@ -59,7 +56,5 @@ object StateMachine:
       }
     
 
-
-  // These are only used at compile-time parsing
     inline def state(name: String): Unit = ()
     inline def initialState(name: String): Unit = ()
